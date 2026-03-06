2a6ad5b4f685e3d3e7cf9b27d4feb751794af8bb
Vishnu Prasannan <vishnuprasannan391@gmail.com>
2026-03-06 17:18:20 +0530

    Overview Almost Done

--- popup.js
+++ popup.js
@@ -593,9 +593,5 @@ function svgWarningSmall(color) {
 }
 
 function svgInfoCircle(color) {
-    return `<svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="12" cy="12" r="10" stroke="${color}" stroke-width="2"/>
-    <path d="M12 16V12" stroke="${color}" stroke-width="2" stroke-linecap="round"/>
-    <circle cx="12" cy="8" r="1" fill="${color}"/>
-  </svg>`;
+    return '';
 }
